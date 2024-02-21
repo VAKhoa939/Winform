1b2ac98da081b26ae8eb383789d0f688ddb7ba91
VAKhoa939 <21110046@student.hcmute.edu.vn>
2024-02-21 11:20:50 +0700

    update 5.0

--- Week02/FHocSinh.cs
+++ Week02/FHocSinh.cs
@@ -24,7 +24,6 @@ namespace Week02
         {
             gvHsinh.DataSource = hocSinhDAO.Load();
             // nhanh cap nhat o day
-            // string sqlStr = "SELECT * FROM HocSinh";
         }
 
         private void btnThem_Click(object sender, EventArgs e)
